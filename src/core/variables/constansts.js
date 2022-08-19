419f0e4e8551b05391e0292778cca88b56bc8ff7
Andrey <gripicha@yandex.by>
2022-08-19 15:51:36 +0300

    fix: gitLinks open in new window

--- src/core/variables/constansts.js
+++ src/core/variables/constansts.js
@@ -1,3 +1,4 @@
 export const AlibekGit = 'https://github.com/limhill'
 export const AndreyGit = 'https://github.com/DittmerOk'
 export const RomanGit = 'https://github.com/roman847'
+export const RsShoolGit = 'https://app.rs.school/'
