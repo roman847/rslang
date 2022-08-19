c3d6e5b090274bb4875c586a7a4caf7a5f39ef22
Andrey <gripicha@yandex.by>
2022-08-19 13:35:45 +0300

    fix: add icon

--- src/pages/main/components/Footer/Footer.tsx
+++ src/pages/main/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import style from './Footer.module.scss'
 import Logo from '../Logo/Logo'
-import { RomanGit, AndreyGit, AlibekGit } from '../../../../core/variables/constansts'
+import { RomanGit, AndreyGit, AlibekGit, SchoolGit } from '../../../../core/variables/constansts'
 
 const Footer: React.FC = () => {
   return (
@@ -56,7 +56,7 @@ const Footer: React.FC = () => {
           </ul>
         </div>
         <div>
-          <a href='https://app.rs.school'>
+          <a href={SchoolGit} target='_blank' rel='noopener noreferrer'>
             <img src='images/school.png'></img>
           </a>
         </div>
---
+++ src/pages/main/components/MainContent/MainContent.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import style from './MainContent.module.scss'
+
+function MainContent() {
+  return (
+    <div className={style.main}>
+      <div>
+        <div>1</div>
+        <div>1</div>
+        <div>1</div>
+      </div>
+      <div>картинка</div>
+    </div>
+  )
+}
+
+export default MainContent
