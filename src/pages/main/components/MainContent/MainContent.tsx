179124a5f9450ffe3e4049b0b00cfdcde8b0b458
Andrey <gripicha@yandex.by>
2022-08-19 18:22:11 +0300

    fix: add mainContent

---
+++ src/pages/main/components/MainContent/MainContent.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import style from './MainContent.module.scss'
+import clsx from 'clsx'
+
+function MainContent() {
+  return (
+    <div className={clsx('container', style.wrapper)}>
+      <div className={style.wrapper__content}>
+        <div className={style.wrapper__title}>
+          Изучай <span>английский </span>
+          вместе с нами
+        </div>
+        <div className={style.wrapper__text}>
+          Эффективное изучение английских слов в игровой форме. Теперь аглийский учить легко и
+          увлекательно. Для детей и взрослых.
+        </div>
+        <div>
+          <button>Красивая кнопка</button>
+        </div>
+      </div>
+      <div className={style.wrapper__avatar}>
+        <img src='images/woman.png' alt='happyWoman' />
+      </div>
+    </div>
+  )
+}
+
+export default MainContent
