b70de7476065bacc4297faf6dd3b59f1b56c3da0
Alibek Zhulmurzin <a.zhulmurzin@gmail.com>
2022-08-21 10:15:42 +0500

    fix: fix path in Main.tsx

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Footer from './components/Footer/Footer'
-import Header from './components/Header/Header'
+import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Opportunities from './components/Opportunities'
 import Team from './components/Team'
