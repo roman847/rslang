bfc803cb9649d0e04c3681ad341ff1bf761ec880
Roman <romabogdan147@gmail.com>
2022-08-23 12:43:10 +0300

    feat: add createUser,getWords (Tue Aug 23 12:43:10 RTZ 2022)

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,16 @@ import AudioCall from 'pages/AudioCall'
 import Sprint from 'pages/Sprint'
 import Statistics from 'pages/Statistics'
 import Textbook from 'pages/Textbook'
+import { createUser } from 'services'
 import Main from '../src/pages/main/Main'
 
 const App = () => {
+  const res = createUser({
+    name: 'Roman',
+    email: 'romanbogdan147@gmail.com',
+    password: '12345678',
+  })
+
   return (
     <BrowserRouter>
       <Routes>
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,1 +1,37 @@
 import axios from 'axios'
+
+export const getWords = async () => {
+  try {
+    const response = await axios.get('https://react-learn-new-words.herokuapp.com/words')
+    return response.data
+  } catch {
+    console.error('Error')
+  }
+}
+
+interface ICreateUser {
+  name: string
+  email: string
+  password: string
+}
+export const createUser = async ({ name, email, password }: ICreateUser) => {
+  try {
+    const response = await axios.post(
+      'https://react-learn-new-words.herokuapp.com/users',
+      {
+        name: name,
+        email: email,
+        password: password,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      },
+    )
+    return response.data
+  } catch {
+    console.error('Error')
+  }
+}
