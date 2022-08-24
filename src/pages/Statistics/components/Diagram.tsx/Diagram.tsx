59ae25b3edb0bf4d0f3ed8a36836dee11a2b88fd
Roman <romabogdan147@gmail.com>
2022-08-24 21:37:11 +0300

    refactor: fix style of StatisticToday (Wed Aug 24 21:37:11 RTZ 2022)

--- src/pages/Statistics/components/Diagram.tsx/Diagram.tsx
+++ src/pages/Statistics/components/Diagram.tsx/Diagram.tsx
@@ -3,21 +3,6 @@ import * as echarts from 'echarts'
 import style from './diagram.module.scss'
 
 const Diagram = () => {
-  const option = {
-    xAxis: {
-      type: 'category',
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-    },
-    yAxis: {
-      type: 'value',
-    },
-    series: [
-      {
-        data: [150, 230, 224, 218, 135, 147, 260],
-        type: 'line',
-      },
-    ],
-  }
   return (
     <div>
       <body>
