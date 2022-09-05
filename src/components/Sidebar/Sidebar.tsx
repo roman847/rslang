9f922953bc4793ae9c5e0a1b48845470a016105c
Alibek Zhulmurzin <a.zhulmurzin@gmail.com>
2022-09-05 15:15:52 +0500

    refactor: delete unused const and move list directly into sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -21,51 +21,6 @@ const Sidebar = () => {
     setOpened((prevState) => !prevState)
   }
 
-  const list = () => (
-    <List sx={styles.list}>
-      <ListSubheader sx={styles.subheader}>
-        <Link component={RouterLink} to={Pages.main} sx={styles.link}>
-          <Logo />
-        </Link>
-        <Box sx={styles.subheader__icon} onClick={toggleDrawer}>
-          <Icon path={IconPath.closeGameIcon} fill='none' stroke='#1D1D1D' strokeWidth='2' />
-        </Box>
-      </ListSubheader>
-      <Box sx={styles.list__items}>
-        <ListItem disablePadding>
-          <Link component={RouterLink} to={Pages.main} sx={styles.item}>
-            Главная
-          </Link>
-        </ListItem>
-        <ListItem disablePadding>
-          <Link component={RouterLink} to={Pages.textbook} sx={styles.item}>
-            Учебник
-          </Link>
-        </ListItem>
-        <List disablePadding>
-          <ListSubheader sx={styles.innerList__subheader}>Игры</ListSubheader>
-          <Box sx={styles.innerList__items}>
-            <ListItem disablePadding>
-              <Link component={RouterLink} to={Pages.sprint} sx={styles.innerList__item}>
-                Спринт
-              </Link>
-            </ListItem>
-            <ListItem disablePadding>
-              <Link component={RouterLink} to={Pages.audio} sx={styles.innerList__item}>
-                Аудиовызов
-              </Link>
-            </ListItem>
-          </Box>
-        </List>
-        <ListItem disablePadding>
-          <Link component={RouterLink} to={Pages.statistics} sx={styles.item}>
-            Статистика
-          </Link>
-        </ListItem>
-      </Box>
-    </List>
-  )
-
   return (
     <Box>
       <Box sx={{ display: opened ? 'none' : 'flex' }}>
@@ -75,7 +30,48 @@ const Sidebar = () => {
         </Link>
       </Box>
       <Drawer anchor='left' open={opened} onClose={toggleDrawer}>
-        {list()}
+        <List sx={styles.list}>
+          <ListSubheader sx={styles.subheader}>
+            <Link component={RouterLink} to={Pages.main} sx={styles.link}>
+              <Logo />
+            </Link>
+            <Box sx={styles.subheader__icon} onClick={toggleDrawer}>
+              <Icon path={IconPath.closeGameIcon} fill='none' stroke='#1D1D1D' strokeWidth='2' />
+            </Box>
+          </ListSubheader>
+          <Box sx={styles.list__items}>
+            <ListItem disablePadding>
+              <Link component={RouterLink} to={Pages.main} sx={styles.item}>
+                Главная
+              </Link>
+            </ListItem>
+            <ListItem disablePadding>
+              <Link component={RouterLink} to={Pages.textbook} sx={styles.item}>
+                Учебник
+              </Link>
+            </ListItem>
+            <List disablePadding>
+              <ListSubheader sx={styles.innerList__subheader}>Игры</ListSubheader>
+              <Box sx={styles.innerList__items}>
+                <ListItem disablePadding>
+                  <Link component={RouterLink} to={Pages.sprint} sx={styles.innerList__item}>
+                    Спринт
+                  </Link>
+                </ListItem>
+                <ListItem disablePadding>
+                  <Link component={RouterLink} to={Pages.audio} sx={styles.innerList__item}>
+                    Аудиовызов
+                  </Link>
+                </ListItem>
+              </Box>
+            </List>
+            <ListItem disablePadding>
+              <Link component={RouterLink} to={Pages.statistics} sx={styles.item}>
+                Статистика
+              </Link>
+            </ListItem>
+          </Box>
+        </List>
       </Drawer>
     </Box>
   )
