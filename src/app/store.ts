af81b6daba19c321b53db0b2477b4343eeb7b13a
Roman <romabogdan147@gmail.com>
2022-09-05 22:23:50 +0300

    fix: store redux (Mon Sep  5 22:23:50 RTZ 2022)

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
 import sprintReducer from 'features/sprint/sprintSlice'
+import textBookReducer from 'redux/textBook/dictionary'
 import counterReducer from 'features/counter/counterSlice'
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     sprint: sprintReducer,
+    textBook: textBookReducer,
   },
 })
 
--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -39,7 +39,6 @@ const SelectElement = ({ label }: IPropsSelectElement) => {
       setOpen(false)
     }
   }
-
   return (
     <Box>
       <Button
